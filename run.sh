3929d928fd0a6402bf5314c15b1b78ffa926e310
Dustin Bailey <bailey.dust@gmail.com>
2021-01-21 15:30:51 -0800

    Makefile was proving to be buggy, using .sh instead

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+export SENTRY_ORG=testorg-az
+export SENTRY_PROJECT=flutter
+export VERSION=`sentry-cli releases propose-version`
+export PREFIX=./obfuscated_symbols
+export ANDROID_SYMBOLS=./build/app/intermediates/cmake/debug/obj/x86_64
+
+flutter build apk --obfuscate --split-debug-info=./obfuscated_symbols
+sentry-cli upload-dif -o $SENTRY_ORG -p $SENTRY_PROJECT --wait $PREFIX
+sentry-cli upload-dif -o $SENTRY_ORG -p $SENTRY_PROJECT --wait $ANDROID_SYMBOLS
+sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $VERSION
+sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $VERSION
+flutter install
