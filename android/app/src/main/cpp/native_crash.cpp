c1e77855e184c72a0b7c4a988f0f9ada52a393c0
Dustin Bailey <bailey.dust@gmail.com>
2021-01-21 11:39:05 -0800

    Removing unnecessary files and small edits to native_crashcpp

--- android/app/src/main/cpp/native_crash.cpp
+++ android/app/src/main/cpp/native_crash.cpp
@@ -1,13 +1,14 @@
 //
 // Created by Dustin Bailey on 1/19/21.
 //
-
+#include <jni.h>
 #include <stdint.h>
 #define TAG "sentry-nativesample"
 
 extern "C" {
 
-JNIEXPORT void JNICALL int32_t native_crash(JNIEnv *env, jclass cls) {
+JNIEXPORT void JNICALL native_crash(JNIEnv *env,jclass cls){
     char *ptr = 0;
         *ptr += 1;
+}
 }
