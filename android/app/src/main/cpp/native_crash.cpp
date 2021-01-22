502225971e8420ce96aa9550f07e005c29e75f4a
Dustin Bailey <bailey.dust@gmail.com>
2021-01-21 16:43:30 -0800

    JNI Naming convention fix

--- android/app/src/main/cpp/native_crash.cpp
+++ android/app/src/main/cpp/native_crash.cpp
@@ -3,12 +3,14 @@
 //
 #include <jni.h>
 #include <stdint.h>
-
+#include <android/log.h>
+#define TAG "sentry-sample"
 
 extern "C" {
 
-JNIEXPORT void JNICALL native_crash(JNIEnv *env,jclass cls){
+JNIEXPORT void JNICALL Java_com_example_sentry_1flutter_1app_MainActivity_crash(JNIEnv *env,jclass cls,jint i){
+//__android_log_print(ANDROID_LOG_WARN, TAG, "About to crash with a SEGFAULT in C++!");
     char *ptr = 0;
-        *ptr += 1;
+    *ptr += 1;
 }
 }
