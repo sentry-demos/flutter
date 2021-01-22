05d6ff81f5186731273f19a688f6beb993b596d8
Dustin Bailey <bailey.dust@gmail.com>
2021-01-22 13:48:42 -0800

    Updating run.sh obfuscation script

--- run.sh
+++ run.sh
@@ -5,14 +5,13 @@ set -e
 export SENTRY_ORG=testorg-az
 export SENTRY_PROJECT=flutter
 export VERSION=android@1.0.0+3
-export PREFIX=./obfuscated_symbols
+export PATH=./obfuscated_symbols
 
 
 # build fat apk & obfuscate, split debug into specified directory
-flutter build apk --obfuscate --split-debug-info=./obfuscated_symbols
+flutter build apk --obfuscate --split-debug-info=$PATH
 
 # directory 'obfuscated/symbols' contain the Dart debug info files but to include platform ones, use current dir.
-sentry-cli upload-dif -o $SENTRY_ORG -p $SENTRY_PROJECT --include-sources --wait ./obfuscated_symbols
 sentry-cli upload-dif -o $SENTRY_ORG -p $SENTRY_PROJECT --include-sources --wait .
 
 # create new sentry release
