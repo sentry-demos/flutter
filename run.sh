bcada602abc46119c54906a69ca1d924b877d377
Dustin Bailey <bailey.dust@gmail.com>
2021-01-22 16:27:36 -0800

    Forgot to remove old versioning in build

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ set -e
 # Setup env variables
 export SENTRY_ORG=testorg-az
 export SENTRY_PROJECT=flutter
-export VERSION=android@1.0.0+3
 export DIR=./obfuscated_symbols
 export SENTRY_RELEASE=`sentry-cli releases propose-version`
 #SENTRY_RELEASE will be used during SDK initialization (for release health) in dart.main & here for associating commits & debug info
@@ -16,10 +15,10 @@ flutter build apk --dart-define=SENTRY_RELEASE=$SENTRY_RELEASE --obfuscate --spl
 sentry-cli upload-dif -o $SENTRY_ORG -p $SENTRY_PROJECT --include-sources --wait .
 
 # create new sentry release
-sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $VERSION
+sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $SENTRY_RELEASE
 
 # associate commits
-sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $VERSION
+sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $SENTRY_RELEASE
 
 #install apk on running emulator
 flutter install
