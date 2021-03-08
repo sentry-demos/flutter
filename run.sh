3ee63daa706fe27959f355059e10025d8a20ba76
Dustin Bailey <bailey.dust@gmail.com>
2021-03-08 10:25:00 -0800

    Updates to build

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 # Setup env variables
 export SENTRY_ORG=testorg-az
 export SENTRY_PROJECT=flutter
-export DIR=./obfuscated_symbols
+export DIR=/obfuscated_symbols
 export SENTRY_RELEASE=`sentry-cli releases propose-version`
 export SENTRY_ENVIRONMENT=staging
 #SENTRY_RELEASE will be used during SDK initialization (for release health) in dart.main & here for associating commits & debug info
@@ -13,7 +13,7 @@ export SENTRY_ENVIRONMENT=staging
 flutter build apk --dart-define=SENTRY_RELEASE=$SENTRY_RELEASE --obfuscate --split-debug-info=$DIR
 
 # directory 'obfuscated/symbols' contain the Dart debug info files but to include platform ones, use current dir.
-sentry-cli upload-dif -o $SENTRY_ORG -p $SENTRY_PROJECT --include-sources --wait . DIR
+sentry-cli upload-dif -o $SENTRY_ORG -p $SENTRY_PROJECT --include-sources --wait . $DIR
 
 # create new sentry release
 sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $SENTRY_RELEASE
